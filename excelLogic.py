6b2fa735a2b7652276fb74a687cc4eefaa0824c1
kienvi <kienvi96@hotmail.com>
2022-10-17 17:29:36 +1000

    Began to flesh out the app steps.

--- excelLogic.py
+++ excelLogic.py
@@ -3,6 +3,11 @@ import pandas as pd
 pathToExcel = "pandas_to_excel.xlsx"
 
 def readFromExcel():
-    userSheet = pd.read_excel(f"{pathToExcel}", sheet_name="user1")
-    userSheetList = userSheet["main-links"].to_list()
+    userSheet = pd.read_excel(f"{pathToExcel}", sheet_name="pryce14")
+    userSheetList = userSheet["outer-links"].to_list()
     return userSheetList
+
+def writeToExcel(arr, colName: str, user: str):
+    df = pd.DataFrame(arr, columns=[f"{colName}"])
+    print(df)
+    df.to_excel(pathToExcel, sheet_name=f"{user}")
--- index.py
+++ index.py
@@ -7,6 +7,7 @@ import time
 from excelLogic import *
 import openpyxl
 
+# Configurations for the selenium web driver
 DRIVER_PATH = 'C:/Users/kienv/Downloads/chromedriver_win32/chromedriver'
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -17,39 +18,45 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH, chrome_options=options)
 artist = "pryce14"
 # artist = "ghost-malone"
 url = f"https://www.deviantart.com/{artist}/gallery/all"
+# Column name referring to the links leading to individual images
+outerLinksColTitle = "outer-links"
+# Column name referring to the links leading to the actual src 
+srcLinksColTitle = "src-links"
 
-# driver.get(url)
+# Going to the artist's page then scanning for a "next" button
+driver.get(url)
+nextButton = ""
+potentialNextButtons = driver.find_elements(By.CLASS_NAME, "_1qdQj")
+for x in range(len(potentialNextButtons)):
+    if potentialNextButtons[x].text == "Next":
+        print(potentialNextButtons[x].text)
+        nextButton = potentialNextButtons[x]
 
-# # Scan page for "next" button
-# nextButton = ""
-# potentialNextButtons = driver.find_elements(By.CLASS_NAME, "_1qdQj")
-# for x in range(len(potentialNextButtons)):
-#     if potentialNextButtons[x].text == "Next":
-#         print(potentialNextButtons[x].text)
-#         nextButton = potentialNextButtons[x]
+# Process the page with bs4
+page_source = driver.page_source
+soup = BeautifulSoup(page_source,"html.parser")
 
-# # Process the page with bs4
-# page_source = driver.page_source
-# soup = BeautifulSoup(page_source,"html.parser")
+# Scan page for images and keep an array off the links
+foundImgArr = []
+for elem in soup.find_all("a", {"data-hook": "deviation_link"}):
+    if elem["href"] != None:
+        foundImgArr.append(elem["href"])
 
-# # Scan page for images and keep an array off the links
-# foundImgArr = []
-# for elem in soup.find_all("a", {"data-hook": "deviation_link"}):
-#     if elem["href"] != None:
-#         foundImgArr.append(elem["href"])
+# Proceeding to next page if required
+time.sleep(2)
+nextButton.click()
 
-# # Proceeding to next page if required
-# time.sleep(2)
-# nextButton.click()
+# Write to excel file. At this stage it would be compiling the found outer links
+writeToExcel(foundImgArr, outerLinksColTitle, artist)
 
-# df = pd.DataFrame(foundImgArr, columns=["main-links"])
-# print(df)
-# df.to_excel(pathToExcel, sheet_name=f"user1")
-
+# Go into the excel file to fetch the stored main links. We need to now send a request to every one of these links to then get it's src link
 excelData = readFromExcel()
 response = requests.get(excelData[0])
 print(response.status_code)
 soup = BeautifulSoup(response.text, 'html.parser')
 print(soup.find_all("div", {"class": "_1X6bR"}))
+
+# Go into the excel file again and for each src link we will download it
+
 
 time.sleep(1200)
